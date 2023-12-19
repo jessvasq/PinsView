55dc69c1bbe6a1cdcc40a4b48fd9a37d63c87120
User Name <jessnsq.codes@gmail.com>
2023-12-19 18:08:21 -0500

    NavBar component

--- frontend-social-media-app/src/container/Pins.jsx
+++ frontend-social-media-app/src/container/Pins.jsx
@@ -2,12 +2,26 @@ import {React, useState } from 'react';
 import {Route, Routes } from 'react-router-dom';
 import { NavBar, Feed, PinDetail, CreatePin, Search } from '../components'
 
-const Pins = () => {
+const Pins = ({user}) => {
   const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <NavBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user} />
+      </div>
 
+      <div className='h-full'> 
+        <Routes>
+          <Route path='/' element={<Feed />}></Route>
+          <Route path='/category/:categoryId' element={<Feed />}></Route>
+          <Route path='/pin-detail/:pinId' element={<PinDetail user={user} />}></Route>
+          <Route path='/createPin' element={<CreatePin user={user} />}></Route>
+          <Route path='/search' element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />}></Route>
+        </Routes>
+      </div>
+
+      
     </div>
   )
 }
